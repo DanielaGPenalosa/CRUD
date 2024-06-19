f4ab5c8c45f979bcf915c38478bd6555b24d6c5f
DanielaGPenalosa <144213497+DanielaGPenalosa@users.noreply.github.com>
2024-06-18 23:54:29 -0700

    Add files via upload

---
+++ crud/fireConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbD2KJdO7XlcDM5r2BPsKcjXydsGdFd4I",
+    authDomain: "crud-5fefd.firebaseapp.com",
+    projectId: "crud-5fefd",
+    storageBucket: "crud-5fefd.appspot.com",
+    messagingSenderId: "226830769355",
+    appId: "1:226830769355:web:b353c70e453df6ef250f4e"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
